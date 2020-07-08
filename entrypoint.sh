8339fa4353c20c2aba4632703671c33d74f090f9
Bart Machielsen <bartmachielsen@gmail.com>
2020-07-08 19:38:14 +0200

    Peronsal token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Sending dependabot notification"
 cd /github/workspace || exit
+echo 'Pushing: name='$(python setup.py --name)', version='$(python setup.py --version)';'
 curl \
   -d '{"name":"'$(python setup.py --name)'", "version":"'$(python setup.py --version)'", "package-manager":"pip"}' \
   -H "Content-Type: application/json" \
