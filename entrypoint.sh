0755d448cee439bc5ac33c36a6bc0cbf8f0387e3
Bart Machielsen <bartmachielsen@gmail.com>
2020-07-08 19:31:23 +0200

    Bearer token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 echo "Sending dependabot notification"
 cd /github/workspace || exit
-echo $(ls)
 curl \
   -d '{"name":"'$(python setup.py --name)'", "version":"'$(python setup.py --version)'", "package-manager":"pip"}' \
   -H "Content-Type: application/json" \
-  -H "Authorization: Personal $INPUT_GITHUB_TOKEN" \
+  -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" \
   -X POST https://api.dependabot.com/release_notifications/private
 echo "Finished sending dependabot notification"
