36fb24968a0611d5ac6bbf53d5fd76140779ca11
Bart Machielsen <bartmachielsen@gmail.com>
2020-07-08 19:19:31 +0200

    Added basic git action details

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/sh -l
+#!/bin/sh
 echo "Sending dependabot notification"
+cd /github/workspace || exit
+cat setup.py
 curl \
-  -d '{"name":"'$(python /github/workspace/setup.py --name)'", "version":"'$(python /github/workspace/setup.py --version)'", "package-manager":"pip"}' \
+  -d '{"name":"'$(python setup.py --name)'", "version":"'$(python setup.py --version)'", "package-manager":"pip"}' \
   -H "Content-Type: application/json" \
-  -H "Authorization: Personal ${{ INPUT_GITHUB_TOKEN }}" \
+  -H "Authorization: Personal $INPUT_GITHUB_TOKEN" \
   -X POST https://api.dependabot.com/release_notifications/private
 echo "Finished sending dependabot notification"
