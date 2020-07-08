14f9050a7d6c9509315d871d75acb3c4b14da1e1
Bart Machielsen <bartmachielsen@gmail.com>
2020-07-08 19:34:40 +0200

    Peronsal token

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ cd /github/workspace || exit
 curl \
   -d '{"name":"'$(python setup.py --name)'", "version":"'$(python setup.py --version)'", "package-manager":"pip"}' \
   -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" \
+  -H "Authorization: Personal $INPUT_GITHUB_TOKEN" \
   -X POST https://api.dependabot.com/release_notifications/private
 echo "Finished sending dependabot notification"
